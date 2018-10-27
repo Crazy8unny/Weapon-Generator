92a23e7cc482b814b8cbd8a38a68624ee4c02c33
CrazyBunny <ededmcb@gmail.com>
2018-10-28 00:38:19 +0300

    feat(Version): First version !

---
+++ Weapon Generator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Weapon_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("RZ Musashi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weapon Generator";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(135, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("RZ Musashi", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(86, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Download!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 182);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(441, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(260, 182);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(777, 375);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Weapon Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Weapon Generator/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Weapon_Generator
+{
+    public partial class Form1 : Form
+    {
+        int rotateTimes = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Image Files(*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            pictureBox1.Image = Image.FromFile(filePath);
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            button1.Visible = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                pictureBox2.Image = RotateImage(new Bitmap(pictureBox1.Image), 18 * rotateTimes);
+                rotateTimes++;
+        }
+
+        private Bitmap RotateImage(Bitmap rotateMe, float angle)
+        {
+            //First, re-center the image in a larger image that has a margin/frame
+            //to compensate for the rotated image's increased size
+
+            //var bmp = new Bitmap(rotateMe.Width + (rotateMe.Width / 2), rotateMe.Height + (rotateMe.Height / 2));
+
+            //using (Graphics g = Graphics.FromImage(bmp))
+            //    g.DrawImageUnscaled(rotateMe, (rotateMe.Width / 4), (rotateMe.Height / 4), bmp.Width, bmp.Height);
+
+            //bmp.Save("moved.png");
+            //rotateMe = bmp;
+
+            //Now, actually rotate the image
+            Bitmap rotatedImage = new Bitmap(rotateMe.Width, rotateMe.Height);
+            Bitmap ss = new Bitmap(rotateMe.Width * 40, rotateMe.Height * 40);
+            //Bitmap backup = rotatedImage;
+
+            using (Graphics t = Graphics.FromImage(ss))
+            {
+                for (int i = 1; i < 5; i++)
+                {
+                    for (int j = 1; j < 11; j++)
+                    {
+                        using (Graphics g = Graphics.FromImage(rotatedImage))
+                        {
+                            t.DrawImage(Rotate(rotateMe, 18 * (i * j)), new Point(rotateMe.Height * j, rotateMe.Width * i)); //draw the image on the new bitmap
+                            //rotatedImage = backup;
+                        }
+                    }
+                }
+            }
+            ss.Save("rotated.png");
+            return ss;
+        }
+
+        private Bitmap Rotate(Bitmap rotateMe, float angle)
+        {
+            //First, re-center the image in a larger image that has a margin/frame
+            //to compensate for the rotated image's increased size
+
+            //var bmp = new Bitmap(rotateMe.Width + (rotateMe.Width / 2), rotateMe.Height + (rotateMe.Height / 2));
+
+            //using (Graphics g = Graphics.FromImage(bmp))
+            //    g.DrawImageUnscaled(rotateMe, (rotateMe.Width / 4), (rotateMe.Height / 4), bmp.Width, bmp.Height);
+
+            //bmp.Save("moved.png");
+            //rotateMe = bmp;
+
+            //Now, actually rotate the image
+            Bitmap rotatedImage = new Bitmap(rotateMe.Width, rotateMe.Height);
+
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform(rotateMe.Width / 2, rotateMe.Height / 2);   //set the rotation point as the center into the matrix
+                g.RotateTransform(angle);                                        //rotate
+                g.TranslateTransform(-rotateMe.Width / 2, -rotateMe.Height / 2); //restore rotation point into the matrix
+                g.DrawImage(rotateMe, new Point(0, 0));                          //draw the image on the new bitmap
+            }
+
+            rotatedImage.Save("rotated.png");
+            return rotatedImage;
+        }
+    }
+}
